e2bf9cac057fe2318696018f9287bd35aff6f3f4
DonnyLVu <donnyvu897@gmail.com>
2020-11-02 21:45:26 -0800

    working postman again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('./lib/client').connect();
 
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 6666;
+const PORT = process.env.PORT || 7744;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
